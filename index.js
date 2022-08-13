c88ab47327ef95d3875d6da403d1126b3595f1c6
akshayL <akshaylilani60@gmail.com>
2022-08-13 14:26:38 +0530

    changed to white

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ particlesJS("particles-js", {
     line_linked: {
       enable: false,
       distance: 150,
-      color: "#e8aa42",
+      color: "#fff",
       opacity: 0.4,
       width: 1,
     },
@@ -103,23 +103,3 @@ particlesJS("particles-js", {
   },
   retina_detect: true,
 });
-
-/* ---- stats.js config ---- */
-
-// var count_particles, stats, update;
-// stats = new Stats();
-// stats.setMode(0);
-// stats.domElement.style.position = "absolute";
-// stats.domElement.style.left = "0px";
-// stats.domElement.style.top = "0px";
-// document.body.appendChild(stats.domElement);
-// count_particles = document.querySelector(".js-count-particles");
-// update = function () {
-//   stats.begin();
-//   stats.end();
-//   if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-//     count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-//   }
-//   requestAnimationFrame(update);
-// };
-// requestAnimationFrame(update);
