5dda4a8fda56c1a2854057a66479ca4936486446
Akshay Lilani <akshaylilani60@gmail.com>
2022-12-18 21:33:25 +0530

    📦 NEW: Card design

---
+++ src/assets/index.jsx
@@ -0,0 +1,13 @@
+import profile from "./profile.png"
+import github from "./github.svg"
+import upwork from "./upwork.svg"
+import tradingview from "./tradingview.svg"
+import curve from "./curve.svg"
+
+export {
+    profile,
+    github,
+    upwork,
+    tradingview,
+    curve
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { profile, tradingview, curve } from "../assets";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { SiUpwork } from "react-icons/si";
+import { FaGithubSquare } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
 import Particlesjs from "../components/Particlejs";
+import { useSpring, animated } from "react-spring";
 
 const Home = () => {
-  
+  const scaleUp = useSpring({from:{transform:"scale(1)"}, to:{transform:"scale(2)"}, reset:true})
+
+  const [linkedinHover, setLikendinHover] = useState(false)
+
   return (
     <>
-      <Particlesjs/>
+      <Particlesjs />
       <div className="home d-flex justify-content-center align-items-center">
-        <h1 className="text-center">
-          Hi 👋🏼, <br />
-          My name is Akshay Lilani
-          <br />
-          and this site is under construction🏗️
-        </h1>
+        <div className="info-card d-flex flex-column justify-content-around align-items-center">
+          <img
+            src={profile}
+            alt="Akshay's profile pic"
+            className="profile-photo"
+          />
+          <img
+            src={curve}
+            alt=""
+            className="position-absolute top-0 w-100 curve"
+          />
+          <div>
+            <h1 className="name mb-0 text-center">Akshaykumar Lilani</h1>
+            <h3 className="sub-heading text-center">
+              Software Engineer at Teson LLP
+            </h3>
+          </div>
+
+          <div className="text-center bio">
+            <p className="mb-0">
+              Building web applications using
+              <br /> <span> ReactJS</span>,<span> Mongodb</span>,{" "}
+              <span> Redux</span> & <span> ExpressJS</span>.
+            </p>
+            <p className="mb-0">
+              <br /> I also have hands-on experience with <span>Elasticsearch</span>, <span>MySQL</span>,
+              {" "}<span>Django</span>, <br/><span>Django REST</span> and <span>Python Scripting</span>
+            </p>
+          </div>
+
+          <div className="">
+            <div className="social-icons d-flex flex-row align-items-center justify-content-center w-100">
+              <a href="https://in.tradingview.com/u/S_Codes/#published-scripts">
+                <img src={tradingview} alt="" className="tradingview" />
+              </a>
+              <a href="https://www.linkedin.com/in/akshay-lilani">
+                <AiOutlineLinkedin size={27}/>
+              </a>
+              <a href="https://www.upwork.com/freelancers/~01c56c58341787db81">
+                <SiUpwork size={27} />
+              </a>
+              <a href="https://github.com/AkshaykumarLilani">
+                <FaGithubSquare size={27} />
+              </a>
+              <a href="https://www.instagram.com/akshay_lilani/">
+                <BsInstagram size={27} />
+              </a>
+            </div>
+            <p className="text-center disclaimer">
+              Please visit my <span>linkedin</span> to see my experience and
+              projects, I will be updating this site soon.
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
