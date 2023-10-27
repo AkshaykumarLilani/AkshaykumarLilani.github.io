cf651cc5a4f2452a85271977e9d8a307c8ae2915
Akshay Lilani <akshaylilani60@gmail.com>
2023-10-27 15:54:11 +0530

    📦 NEW: Add NavBar and module css

---
+++ src/Components/Particlejs.jsx
@@ -0,0 +1,89 @@
+import React, { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import css from "./GlassCard.module.css";
+
+const Particlesjs = () => {
+    const particlesInit = useCallback(async (engine) => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async (container) => {
+        await console.log(container);
+    }, []);
+
+    return (
+        <Particles
+            id={css["tsparticles"]}
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: false,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: false,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 40,
+                        },
+                        repulse: {
+                            distance: 100,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        directions: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 1,
+                        straight: true,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 5000,
+                        },
+                        value: 800,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "star",
+                    },
+                    size: {
+                        value: { min: 1, max: 1 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+    )
+}
+
+export default Particlesjs;
