7fceed83e61de14c5561b083197185c542b79cb1
Akshay Lilani <akshaylilani60@gmail.com>
2023-12-10 21:30:35 +0530

    👌 IMPROVE: Add netlify to footer

--- src/Components/Common/Footer.jsx
+++ src/Components/Common/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className='d-flex flex-column justify-content-center align-items-center p-5 w-100 mt-5 mb-5 mb-md-0'>
             <p className='mb-0 text-center'>Developed by Akshaykumar Lilani</p>
-            <p className='mb-0 text-center'>Deployed on Github Pages</p>
+            <p className='mb-0 text-center'>Deployed on Github Pages/Netlify</p>
         </footer>
     )
 }
