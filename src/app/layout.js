8f57a2373b5ad2e156aa7558635001ded649ee05
Teson <Akshay Lilani>
2025-01-29 13:34:55 +0530

    Update resume and fix favicon error

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" type="image/svg+xml" href="/profile.ico" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
         <meta property="og:title" content="Akshaykumar Lilani" />
         <meta property="og:type" content="Portfolio" />
         <meta property="og:url" content="https://akshaylilani.com" />
